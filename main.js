07ff605591a66d4d67c0ca8d8cc39525dfd94065
Peter Smolenko <smolenko.peter@gmail.com>
2020-02-09 16:12:31 +0300

    fixed animation

--- main.js
+++ main.js
@@ -2,17 +2,19 @@ window.addEventListener('load', ()=>{
     let completeAnimate = true;
 
     function animate(type, el, duration, frequency = 50){
+        console.time('run_time');
         completeAnimate = false;
         let curHeight = type === "show"?0:type==="hide"?el.scrollHeight:null;
-
+        console.log('f:', curHeight);
         if (curHeight != null) {
             let animateID = setInterval(()=>{
-                let pixDelta = el.scrollHeight/(duration/(1000/frequency));
+                let pixDelta = el.scrollHeight/Math.floor(duration/(1000/frequency));
             
                 if (type === "show"){
                     curHeight += pixDelta;
                 } else {
                     curHeight -= pixDelta;
+                    console.log(curHeight, pixDelta);
                 };
 
                 el.style.maxHeight = curHeight + "px";
@@ -21,36 +23,40 @@ window.addEventListener('load', ()=>{
                     if (type==="hide") el.style.maxHeight = 0 + "px";
                     clearInterval(animateID);
                     completeAnimate = true;
+                    console.timeEnd('run_time');
                 }
             }, 1000/frequency)
-        }
-    
-  }
-  document.querySelector('.acdn .acdn__item> .acdn__inner').style.maxHeight = document.querySelector('.acdn .acdn__item> .acdn__inner').scrollHeight + "px";
-  Object
-    .values(document.querySelectorAll('.acdn__toggler'))
-    .forEach(el=>{
-        el.addEventListener('click', e=>{
-            e.preventDefault();
+        } 
+    }
+
+    document.querySelector('.acdn .acdn__item> .acdn__inner').style.maxHeight = document.querySelector('.acdn .acdn__item> .acdn__inner').scrollHeight + "px";
+    document.querySelector('.acdn .acdn__item').classList.add('active');
+
+    document.querySelector('.acdn').addEventListener('click', e=>{
+        e.preventDefault();
+        if(e.target.classList.contains('acdn__toggler') || e.target.parentNode.classList.contains('acdn__toggler')){
+            let el = e.target.classList.contains('acdn__toggler')?e.target:e.target.parentNode;
             let item = el.parentNode,
             items = item.parentNode.children,
             content = item.querySelector('.acdn__inner'),
-            duration = 200,
-            frequency = 50,
+            duration = 300,
+            frequency = 10,
             activeEl = Object
                 .values(items)
                 .find(el=>el.classList.contains('active')?el:null);
 
             if (completeAnimate === true) {
                 if (!Object.is(item, activeEl)) {
+                    if (activeEl){
+                        activeEl.classList.remove('active')
+                        activeEl.querySelector('.acdn__toggler-triangle').innerHTML = "&#9660;";
+                        animate('hide', activeEl.querySelector('.acdn__inner'), duration, frequency);
+                    }
+                    animate('show', content, duration, frequency);
                     item.classList.add('active');
                     el.querySelector('.acdn__toggler-triangle').innerHTML = "&#9650;";
-                    animate('hide', activeEl.querySelector('.acdn__inner'), duration, frequency);
-                    animate('show', content, duration, frequency);
-                    activeEl.classList.remove('active')
-                    activeEl.querySelector('.acdn__toggler-triangle').innerHTML = "&#9660;";
                 }
             }
-        })
+        } else {console.log(e.target);}
     })
 })
